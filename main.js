3f828c7a6d02c8a19853d902d86ae86b3d7a3019
Shohei Kikuchi <killchance2256@gmail.com>
2021-10-26 17:09:11 +0900

    js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-// コメント
-console.log('hello');
+const form = document.getElementById("form");
+const input =document.getElementById("input");
+const ul = document.getElementById("ul");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(input.value);
+  add();
+});
+
+function add() {
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  li.classList.add("list-gropu-item");
+  ul.appendChild(li);
+}
