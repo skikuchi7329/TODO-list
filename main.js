9cb38833c8108883029edf3a29b5f3de40d2e8cb
Shohei Kikuchi <killchance2256@gmail.com>
2021-10-29 13:33:44 +0900

    ページをリロードしてもデータが残るように変更

--- main.js
+++ main.js
@@ -2,19 +2,42 @@ const form = document.getElementById("form");
 const input =document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   console.log(input.value);
   add();
 });
 
-function add() {
+function add(todo) {
   let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
+  }
+
   if (todoText) {
   const li = document.createElement("li");
   li.innerText = todoText;
-  li.classList.add("list-gropu-item");
+  li.classList.add("list-group-item");
   ul.appendChild(li);
   input.value ="";
+  saveData();
   }
-}
+};
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list => {
+    todos.push(list.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
