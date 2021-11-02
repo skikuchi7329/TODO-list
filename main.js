9dc30fa42b3fc4b92b01d398c413e0d7bb7d397a
Shohei Kikuchi <killchance2256@gmail.com>
2021-11-02 20:50:30 +0900

    リロードをしても打ち消し線が消えないように修正

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 if (todos) {
   todos.forEach(todo => {
     add(todo);
-  })
-}
+  });
+};
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
@@ -20,13 +20,17 @@ function add(todo) {
   let todoText = input.value;
 
   if (todo) {
-    todoText = todo;
+    todoText = todo.text;
   }
 
   if (todoText) {
   const li = document.createElement("li");
   li.innerText = todoText;
   li.classList.add("list-group-item");
+
+  if (todo && todo.completed) {
+    li.classList.add("text-decoration-line-through")
+  }; 
 
   li.addEventListener("contextmenu", function 
   (event) {
@@ -35,6 +39,12 @@ function add(todo) {
     saveData();
     
   });
+
+  li.addEventListener("click", function() {
+    li.classList.toggle("text-decoration-line-through");
+    saveData();
+  });
+
   ul.appendChild(li);
   input.value ="";
   saveData();
@@ -44,8 +54,13 @@ function add(todo) {
 function saveData() {
   const lists = document.querySelectorAll("li");
   let todos = [];
+  
   lists.forEach(list => {
-    todos.push(list.innerText);
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through")
+    };
+    todos.push(todo);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
 };
